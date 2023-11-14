bcc48911e34682184c1641dcd64a81d9e94a990d
Stefan Mermans <stefanmermans99@gmail.com>
2023-11-15 00:53:33 +0100

    Resp wip

--- src/client.rs
+++ src/client.rs
@@ -11,23 +11,6 @@ fn get_stream(host: &str, port: &str) -> TcpStream {
     TcpStream::connect(format!("{}:{}", host, port)).unwrap()
 }
 
-fn send_message(stream: &mut TcpStream, data: &[u8]) -> Vec<u8> {
-    stream.write_all(data).unwrap();
-
-    // TODO: We need to be able to get larger buffers, probably by reading multiple times and
-    // combining the buffers
-    let mut buffer = [0; 512];
-    let size = stream.read(&mut buffer).unwrap();
-
-    buffer[..size].to_vec()
-}
-
-fn send_message_string(stream: &mut TcpStream, data: String) -> String {
-    let response = send_message(stream, data.as_bytes());
-
-    String::from_utf8_lossy(&response).to_string()
-}
-
 impl Client {
     pub fn connect(host: &str, port: &str) -> Self {
         let stream = get_stream(host, port);
@@ -36,13 +19,25 @@ impl Client {
     }
 
     pub fn handshake(&mut self) {
-        let response = send_message_string(&mut self.stream, "HELLO 3\r\n".to_string());
+        let response = self.send_message_string("HELLO 3\r\n".to_string());
 
-        println!(
-            "===REDIS===\n{}\n===END REDIS===",
-            response,
-        );
+        println!("===REDIS===\n{}\n===END REDIS===", response,);
+    }
+
+    pub fn send_message(&mut self, data: &[u8]) -> Vec<u8> {
+        self.stream.write_all(data).unwrap();
+
+        // TODO: We need to be able to get larger buffers, probably by reading multiple times and
+        // combining the buffers
+        let mut buffer = [0; 512];
+        let size = self.stream.read(&mut buffer).unwrap();
+
+        buffer[..size].to_vec()
+    }
+
+    pub fn send_message_string(&mut self, data: String) -> String {
+        let response = self.send_message(data.as_bytes());
+
+        String::from_utf8_lossy(&response).to_string()
     }
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::client::Client;
 
 mod client;
+mod resp;
 
 const HOST: &str = "127.0.0.1";
 const PORT: &str = "6379";
@@ -8,5 +9,7 @@ const PORT: &str = "6379";
 fn main() {
     let mut client = Client::connect(HOST, PORT);
     client.handshake();
-}
 
+    let response = client.send_message_string("EVAL \"return { double = tonumber(ARGV[1]) }\" 0 15.1e0\r\n".to_string());
+    println!("response\n{}", &response);
+}
